c6201ddcac26ec4d9e0f447421ad4dc888e3487c
German Vicentin <92609839+gervic08@users.noreply.github.com>
2025-09-19 16:20:17 -0300

    Create api.rb

---
+++ config/routes/api.rb
@@ -0,0 +1,10 @@
+namespace :api, defaults: {format: :json}, path: nil do
+  namespace :v1 do
+    resource :auth, only: [:create, :destroy] do
+      collection do
+        post :refresh
+      end
+      
+    resources :payments, only: [:create]
+  end
+end
